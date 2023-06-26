da968ee10e83538498f56275472db70530ebf14c
alfarishi-git <ralfarishi@gmail.com>
2023-06-26 16:37:32 +0700

    remove: duplicate error message

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -8,8 +8,6 @@
     </div>
     <div class="row">
         <div class="col-md-12">
-            @include('includes.admin.alerts')
-
             {{Form::model($post, ['route' => ['artikel.update', $post],'method'=>'PATCH','files'=>true])}}
             @include('admin.posts.form')
             <div class="form-group text-center">
