da8d555530bc2a4877600aa34d9b0871970b35aa
alfarishi-git <62209112+ralfarishi@users.noreply.github.com>
2023-09-28 14:45:00 +0700

    fix: admin page responsive

--- resources/views/includes/admin_v2/scripts.blade.php
+++ resources/views/includes/admin_v2/scripts.blade.php
@@ -4,9 +4,9 @@
 <script src="{{ asset('admin_v2/js/jquery-ui.min.js') }}"></script>
 <script src="{{ asset('admin_v2/js/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('admin_v2/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('admin_v2/js/dataTables.responsive.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <!-- Bootstrap 4 -->
-{{-- <script src="https://unpkg.com/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script src="{{ asset('admin_v2/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('admin_v2/js/adminlte.js') }}"></script>
