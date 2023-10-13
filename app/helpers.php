b89c53d6bfdb2c4d7fec4a1013bd960acc372783
Al Farishi <62209112+ralfarishi@users.noreply.github.com>
2023-10-13 17:28:36 +0700

    change: helpers file

---
+++ app/helpers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+
+use App\Models\Posts;
+use App\Models\Categories;
+
+function formatDate($date)
+{
+  $carbonDate = Carbon::parse($date);
+
+  $carbonDate->setLocale('id');
+
+  return $carbonDate->isSameDay(Carbon::now()) ?
+    $carbonDate->diffForHumans() :
+    $carbonDate->format('l, d F Y');
+}
+
+
+function getSidebarData()
+{
+  $categories = Categories::withCount(['posts' => function ($query) {
+    $query->where('active', 1);
+  }])->get();
+
+  $tags = Posts::where('active', 1)->pluck('tags')->flatMap(function ($tags) {
+    return explode(',', $tags);
+  })->unique()->reject(function ($tag) {
+    return empty($tag);
+  });
+
+  return compact('categories', 'tags');
+}
