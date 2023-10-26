dda577df303574af9133ae0802b66875d07f55c9
Al Farishi <62209112+ralfarishi@users.noreply.github.com>
2023-10-26 22:04:17 +0700

    remove: unused js code

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -2,22 +2,3 @@
 <script type="text/javascript" src="{{ asset('js/artikel_v2/main.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/artikel_v2/aos.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
-
-{{-- <script type="text/javascript">
-    jQuery(document).ready(function ($) {
-            $(".scroll").click(function (event) {
-                event.preventDefault();
-                $('html,body').animate({
-                    scrollTop: $(this.hash).offset().top
-                }, 1000);
-            });
-        });
-</script> --}}
-
-<!-- script-for-menu -->
-{{-- <script>
-    $("span.menu").click(function () {
-            $(" ul.navig").slideToggle("slow", function () {});
-        });
-</script> --}}
-<!-- script-for-menu End-->
