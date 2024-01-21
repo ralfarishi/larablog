1cd43d2c67249b77bb775cbcaa2bf966be988554
Al Farishi <62209112+ralfarishi@users.noreply.github.com>
2024-01-21 14:25:11 +0700

    Update: user migration

--- database/migrations/2023_10_22_030208_add_role_column_in_user_table.php
+++ database/migrations/2023_10_22_030208_add_role_column_in_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
 	public function up(): void
 	{
 		Schema::table('users', function (Blueprint $table) {
-			$table->enum('role', ['admin', 'writter'])->after('email')->default('writter');
+			$table->enum('role', ['admin', 'writter', 'user'])->after('email');
 		});
 	}
 
